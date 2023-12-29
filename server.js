e34c24f64b0bc41beb1d9ef3a5c4cb51dba88e55
KSH <kangsenghun@hanmail.net>
2023-12-29 15:14:45 +0900

    4.0 Setup

---
+++ server.js
@@ -0,0 +1,15 @@
+import {ApolloServer, gql} from "apollo-server";
+
+//package.json에서 type:"module"로 해야 위에처럼 import로 가능하고, 아니면 아래처럼 require 써야함
+// const {ApolloServer, gql} = require("apollo-server")
+const typeDefs = gql`
+type Query {
+text: String
+hello: String
+}
+`
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+console.log(`🚀 Server ready at ${url}`);
+});
