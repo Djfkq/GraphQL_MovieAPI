256895d6bb7f55a103dc0ff9441054c8f2bf42a5
KSH <kangsenghun@hanmail.net>
2023-12-29 15:25:32 +0900

    4.1 Query Type

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import {ApolloServer, gql} from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 
 //package.json에서 type:"module"로 해야 위에처럼 import로 가능하고, 아니면 아래처럼 require 써야함
 // const {ApolloServer, gql} = require("apollo-server")
 const typeDefs = gql`
-type Query {
-text: String
-hello: String
-}
-`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
 const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
-console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
