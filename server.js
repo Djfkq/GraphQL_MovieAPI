efd806f982d9e6450eadf420476a5b218eeabe3b
KSH <kangsenghun@hanmail.net>
2023-12-29 15:42:22 +0900

    4.2 Scalar and Root Types

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ import { ApolloServer, gql } from "apollo-server";
 //package.json에서 type:"module"로 해야 위에처럼 import로 가능하고, 아니면 아래처럼 require 써야함
 // const {ApolloServer, gql} = require("apollo-server")
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
-    hello: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
   }
 `;
 
